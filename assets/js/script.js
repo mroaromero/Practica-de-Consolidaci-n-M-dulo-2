ea8de83a3cae33e0abd8f02432db5060664db61e
Manuel Roa <mroaromero@gmail.com>
2023-05-17 21:23:07 -0400

    Agregado comentarios al código

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,72 +1,66 @@
-$(document).ready(function() {
-    const cuerpoTabla = $("#cuerpoTabla");
-    let datosOriginales = [];
+$(document).ready(function() {  // La función se ejecutará una vez que el documento HTML haya sido completamente cargado
+    const cuerpoTabla = $("#cuerpoTabla");  // Declaración de Variables
+    let datosOriginales = []; // Array
   
-    // Cargar datos desde la API
-    function cargarTabla() {
+    function cargarTabla() {  // Cargar datos desde la API con ajax
       $.ajax({
         type: "get",
         url: "https://digimon-api.vercel.app/api/digimon",
         dataType: "json",
-        success: function(response) {
-          datosOriginales = response;
-          tabla(response);
+        success: function(response) { // Cuando la respuesta del servidor se recibe correctamente se ejecuta la función
+          datosOriginales = response; // "response" contiene los datos obtenidos de una solicitud AJAX
+          tabla(response);  // Generar y muestrauna una tabla en el documento HTML, utilizando los datos de respuesta recibidos
         }
       });
     }
   
-    // Mostrar imagen en el modal
-    $("#cuerpoTabla").on("click", "img", function() {
-      const imgSrc = $(this).attr("src");
+    $("#cuerpoTabla").on("click", "img", function() { // Evento click para mostrar la imagen del modal
+      const imgSrc = $(this).attr("src"); // Obtiene la ruta de la imagen del elemento actual, utilizando "this" para referirse al elemento
       const modal = $("#miModal");
       const modalImg = $("#img01");
-      modalImg.attr("src", imgSrc);
+      modalImg.attr("src", imgSrc);       // Utiliza el metodo .attr() de jQuery para obtener el valor del atributo "src" del elemento seleccionado
       const captionText = $("#caption");
-      captionText.html($(this).closest("tr").find("td:eq(0)").text());
-      modal.show();
-    });
+      captionText.html($(this).closest("tr").find("td:eq(0)").text());  // Encuentra la fila padre más cercana del elemento actual, busca el primer elemento de la celda en esa fila y establece el contenido HTML de "captionText" con el texto obtenido de ese elemento
+      modal.show(); // Muestra un elemento oculto en la interfaz gráfica con el método .show() de jQuery
+    }); 
   
-    // Cerrar modal
-    $(".close").click(function() {
+    $(".close").click(function() {  // Evento click en los elementos con clase "close" para ocultar el elemento modal con el ID "miModal"
       $("#miModal").hide();
     });
   
-    // Ordenar tabla alfabéticamente
-    function ordenAlfabetico() {
-      const filas = $("#cuerpoTabla tr").get();
-      filas.sort(function(a, b) {
-        const aText = $(a).find("td:eq(0)").text().toUpperCase();
-        const bText = $(b).find("td:eq(0)").text().toUpperCase();
-        return aText.localeCompare(bText);
+    function ordenAlfabetico() {  // Función para ordenar tabla alfabéticamente
+      const filas = $("#cuerpoTabla tr").get(); // Selecciona y obtiene todas las filas de la tabla, generando un arreglo con los elementos seleccionados con el método .get() de jQuery
+      filas.sort(function(a, b) { // Ordena el array de las filas por medio de la función
+        const aText = $(a).find("td:eq(0)").text().toUpperCase(); // Extrae y almacena el texto en mayúsculas del primer elemento de la columna de una tabla
+        const bText = $(b).find("td:eq(0)").text().toUpperCase(); // para el elemento (a) como para el elemento (b)
+        return aText.localeCompare(bText);  // Comparación alfabética entre las cadenas de texto y devuelve un valor numérico que representa su orden
       });
       $.each(filas, function(index, fila) {
         cuerpoTabla.append(fila);
-      });
+      }); // itera sobre cada elemento del arreglo filas con el método .each() de jQuery y agrega cada elemento al final del contenido de la tabla para reordenarla
     }
   
-    // Ordenar tabla por nivel
-    function ordenarPorNivel() {
+    function ordenarPorNivel() {  //Función para ordenar la tabla por nivel de cada Digimon
       const niveles = ["Mega", "Ultimate", "Armor", "Champion", "Rookie", "Training", "In Training", "Fresh"];
       const filas = cuerpoTabla.find("tr").get();
       filas.sort(function(a, b) {
         const aNivel = $(a).find("td:eq(2)").text();
         const bNivel = $(b).find("td:eq(2)").text();
         return niveles.indexOf(aNivel) - niveles.indexOf(bNivel);
-      });
+      }); // Calcula la diferencia de índices entre dos elementos en el arreglo niveles, permitiendo ordenar los elementos de acuerdo a su posición en el arreglo
       $.each(filas, function(index, fila) {
         cuerpoTabla.append(fila);
       });
     }
-  
-    // Generar tabla con los datos
-    function tabla(datos) {
+
+    function tabla(datos) { // Función para generar la tabla con los datos
       cuerpoTabla.html("");
   
-      if (!Array.isArray(datos)) {
+      if (!Array.isArray(datos)) {  // Verifica y convierte (datos) en arreglo para asegurar que sea válido en la función
         datos = [datos];
       }
   
-      datos.forEach(function(dato) {
+      datos.forEach(function(dato) {  // Itera sobre cada elemento del arreglo y crea una fila en la tabla con los valores de cada elemento
         const fila = $("<tr></tr>");
         const columna1 = $("<td></td>");
         const columna2 = $("<td></td>");
@@ -83,27 +77,23 @@ $(document).ready(function() {
   
     cargarTabla();
   
-    // Ordenar alfabéticamente al hacer clic en el botón
-    $("#BtnNombre").click(function() {
+    $("#BtnNombre").click(function() {  // Ordena alfabéticamente al hacer click en el botón
       ordenAlfabetico();
     });
   
-    // Ordenar por nivel al hacer clic en el botón
-    $("#BtnNivel").click(() => {
+    $("#BtnNivel").click(() => {  // Ordena por nivel al hacer click en el botón
       ordenarPorNivel();
     });
     
-    // Filtrar tabla en tiempo real
-    $("#inputBuscar").on("input", function() {
+    $("#inputBuscar").on("input", function() {  // Filtra la tabla en tiempo real
       filtrarTabla();
     });
     
-    // Filtrar tabla por nombre
-    function filtrarTabla() {
-      const inputValor = $("#inputBuscar").val().toLowerCase();
+    function filtrarTabla() { // Función para filtrar tabla por nombre
+      const inputValor = $("#inputBuscar").val().toLowerCase(); // Obtiene el valor del campo de entrada con el método .val() de jQuery y lo convierte a minúsculas.
       const datosFiltrados = datosOriginales.filter(function(dato) {
         return dato.name.toLowerCase().includes(inputValor);
-      });
+      }); // Filtra los datos según el nombre que coincida con el método .filter(), ignorando mayúsculas y minúsculas con el valor de búsqueda
       tabla(datosFiltrados);
     }
 })
